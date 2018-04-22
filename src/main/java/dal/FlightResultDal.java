abc7a888064a10a6aece49e5fe44a8d3831b1a36
giordanolucas <lucas.giordano@mulesoft.com>
2018-04-22 12:11:38 -0300

    Added missing classes

---
+++ src/main/java/dal/FlightResultDal.java
@@ -0,0 +1,48 @@
+package dal;
+
+import com.google.gson.Gson;
+import model.internal.FlightResult;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class FlightResultDal {
+    private static Gson gson = new Gson();
+
+    public static void saveFlightResult(FlightResult flightResult) throws SQLException {
+        try(Connection connection = DatabaseConnectionService.getConnection()){
+            String sql = "INSERT INTO flightResult (idFlightQuery, price, airline, timeCombinationsJson)" +
+                    "VALUES (?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, flightResult.getFlightQuery().getIdFlightQuery());
+            preparedStatement.setDouble(2, flightResult.getPrice());
+            preparedStatement.setString(3, flightResult.getAirline());
+            preparedStatement.setString(4, gson.toJson(flightResult.getTimeCombinations()));
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public static void saveFlightResultBatch(List<FlightResult> flightResultBatch) throws SQLException {
+        try(Connection connection = DatabaseConnectionService.getConnection()){
+            String sql = "INSERT INTO flightResult (idFlightQuery, price, airline, timeCombinationsJson)" +
+                    "VALUES (?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            for(FlightResult flightResult : flightResultBatch){
+                preparedStatement.setInt(1, flightResult.getFlightQuery().getIdFlightQuery());
+                preparedStatement.setDouble(2, flightResult.getPrice());
+                preparedStatement.setString(3, flightResult.getAirline());
+                preparedStatement.setString(4, gson.toJson(flightResult.getTimeCombinations()));
+
+                preparedStatement.addBatch();
+            }
+
+            preparedStatement.executeBatch();
+        }
+    }
+}
---
+++ src/main/java/model/internal/FlightResult.java
@@ -0,0 +1,91 @@
+package model.internal;
+
+import com.google.gson.Gson;
+import model.am.Choice;
+import model.am.Cluster;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FlightResult {
+
+    private Integer idFlightResult;
+    private FlightQuery flightQuery;
+    private Double price;
+    private String airline;
+    private List<TimeCombination> timeCombinations = new LinkedList<>();
+    private LocalDateTime resultDate;
+
+    public FlightResult(Integer idFlightResult, FlightQuery flightQuery, Double price, String airline, LocalDateTime resultDate, String flightCombinationsJson){
+        this.idFlightResult = idFlightResult;
+        this.flightQuery = flightQuery;
+        this.price = price;
+        this.airline = airline;
+        this.timeCombinations = (new Gson()).fromJson(flightCombinationsJson, this.timeCombinations.getClass());
+        this.resultDate = resultDate;
+    }
+
+    public FlightResult(FlightQuery flightQuery, Cluster cluster){
+        this.flightQuery = flightQuery;
+        this.price = cluster.getPrice().getTotal();
+        this.airline = cluster.getSegments().get(0).getChoices().get(0).getLegs().get(0).getMarketingCarrier().getName();
+
+
+        List<Choice> firstFlightChoices = cluster.getSegments().get(0).getChoices();
+        List<Choice> secondFlightChoices = cluster.getSegments().get(1).getChoices();
+
+        for(Choice choiceFirstFlight : firstFlightChoices){
+            FlightTimes time1 = new FlightTimes(choiceFirstFlight.getDepartureDate(),
+                                                choiceFirstFlight.getDepartureTime(),
+                                                choiceFirstFlight.getArrivalDate(),
+                                                choiceFirstFlight.getArrivalTime(),
+                                                choiceFirstFlight.getFlightDuration());
+
+            for(Choice choiceSecondFlight : secondFlightChoices){
+                FlightTimes time2 = new FlightTimes(choiceSecondFlight.getDepartureDate(),
+                                                    choiceSecondFlight.getDepartureTime(),
+                                                    choiceSecondFlight.getArrivalDate(),
+                                                    choiceSecondFlight.getArrivalTime(),
+                                                    choiceSecondFlight.getFlightDuration());
+
+                timeCombinations.add(new TimeCombination(Arrays.asList(time1, time2)));
+            }
+        }
+    }
+
+    public FlightQuery getFlightQuery() {
+        return flightQuery;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getPriceString() {
+        DecimalFormat twoPlaces = new DecimalFormat("0.00");
+        return twoPlaces.format(price);
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public List<TimeCombination> getTimeCombinations() {
+        return timeCombinations;
+    }
+
+    public Integer getIdFlightResult() {
+        return idFlightResult;
+    }
+
+    public void setIdFlightResult(Integer id) {
+        this.idFlightResult = id;
+    }
+
+    public LocalDateTime getResultDate() {
+        return resultDate;
+    }
+}
