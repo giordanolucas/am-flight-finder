6236a783ab391fbc9871ec8c92ce213575932e28
giordanolucas <lucas.giordano@mulesoft.com>
2018-04-23 14:28:05 -0300

    New alert prices

--- src/main/java/scrapper/Program.java
+++ src/main/java/scrapper/Program.java
@@ -10,10 +10,10 @@ import java.util.concurrent.ExecutionException;
 public class Program {
     public static void main (String [] arguments) throws ExecutionException, InterruptedException {
         List<FlightSearch> searches = new ArrayList<>();
-        searches.add(new FlightSearch("BUE", "TYO", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 25000d));
+        searches.add(new FlightSearch("BUE", "TYO", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 20000d));
         searches.add(new FlightSearch("BUE", "BKK", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 20000d));
-        searches.add(new FlightSearch("BUE", "MAD", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 20000d));
-        searches.add(new FlightSearch("BUE", "BCN", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 20000d));
+        searches.add(new FlightSearch("BUE", "MAD", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 19000d));
+        searches.add(new FlightSearch("BUE", "BCN", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 19000d));
         searches.add(new FlightSearch("BUE", "KUL", LocalDate.of(2018, 10, 1), LocalDate.of(2019, 4, 1), 15, 19, 20000d));
 
         SearchSchedulerRunner schedulerRunner = new SearchSchedulerRunner(searches);
